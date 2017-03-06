b616b2d5bf5d5c90f33ca8b5c60010bb9db02700
Robert Richter <robert.c.richter@gmail.com>
2017-03-05 21:04:55 -0600

    Read from Telnet into a channel
    instead of directly from a reader.

--- src/horizons/telnet_client.clj
+++ src/horizons/telnet_client.clj
@@ -1,8 +1,10 @@
 (ns horizons.telnet-client
+  (:require [clojure.core.async :refer [chan sliding-buffer <!! >!!]])
   (:import (org.apache.commons.net.telnet TelnetClient)
            (java.io BufferedReader BufferedWriter InputStreamReader OutputStreamWriter PrintStream)
            (java.nio.charset StandardCharsets Charset)))
 
+(def from-telnet (chan (sliding-buffer 1000)))
 (defn reader [^TelnetClient client]
   (new BufferedReader (new InputStreamReader (.getInputStream client) StandardCharsets/US_ASCII)))
 
@@ -20,7 +22,9 @@
       (try
 
         (while [(.isAvailable client)
-                (while (.ready reader) (.write System/out ^int (.read reader)))
+                (while (.ready reader)
+                  (>!! from-telnet (.read reader))
+                  (.write System/out (<!! from-telnet)))
                 (while (.ready stdin) (.write writer ^int (.read stdin)))
                 (.flush writer)
                 (.flush System/out)])
