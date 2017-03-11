28f7d13ca862d383f5b773d6f2c5636e7dbf7a02
Robert Richter <robert.c.richter@gmail.com>
2017-03-11 17:10:52 -0600

    Add a link to Earth on the main page, and handle errors better

--- src/horizons/core.clj
+++ src/horizons/core.clj
@@ -9,13 +9,19 @@
             [compojure.route :as route]))
 
 (def supported-bodies
-  #{499})
+  #{499 399})
 
 (defn supported? [id]
   (contains? supported-bodies (int (bigdec id))))
 
+(defn build-error-response [body-string failure]
+  (do
+    (throw (Exception. (str "Unable to parse HORIZONS response. While parsing:\n\n" body-string
+                            "\n\nGot the following failure: \n" (with-out-str (print failure)))))))
+
 (defn get-planetary-body [id]
-  (let [body-data (parse (get-body id))]
-    (if (instaparse.core/failure? body-data)
-      body-data
-      (:S (restructure body-data)))))
+  (let [body-string (get-body id)
+        body-parse-tree (parse body-string)]
+    (if (instaparse.core/failure? body-parse-tree)
+        (build-error-response body-string body-parse-tree)
+        (:S (restructure body-parse-tree)))))
