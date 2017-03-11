1087c8e4202fadbae3490a46cfd834858a8c4264
Robert Richter <robert.c.richter@gmail.com>
2017-03-11 14:13:51 -0600

    Process the parse tree into a map

--- src/horizons/parser.clj
+++ src/horizons/parser.clj
@@ -5,22 +5,23 @@
 (def parse
   (core/parser (clojure.java.io/resource "horizons.bnf")))
 
-(defn all-members-have-same-keys?
-  [form]
-  (->> form
-      rest
-      (map #(apply hash-map %))
-      (apply =)))
+(def transform-rules
+  {:ephemeris (fn [& rest] {:ephemeris (set rest)})
+   :ephemeris-line-item (fn [& rest] (into {} rest))
+   :measurement-time (fn [& rest] {:measurement-time (into {} rest)})})
+
+;; If we could give insta/transform a default rule, it should
+;; be (fn [& rest] {:label (into {} rest)}). But alas...
+
+(defn transform
+  [tree]
+  (transform/transform transform-rules tree))
 
 (defn coll-of-colls?
   [form]
   (and
-    (coll? form)
-    (every? coll? (rest form))))
-
-(defn set-of-maps
-  [& args]
-  (set (map #(apply hash-map %) args)))
+    (vector? form)
+    (every? #(and (coll? %) (not (set? %))) (rest form))))
 
 (defn tree->map
   "Transforms a parse tree into a nested map, where the first entry in a non-leaf node becomes the key,
@@ -37,3 +38,9 @@
         (coll-of-colls? form) {(first form) (into {} (rest form))}
         :else form))
    tree))
+
+(defn restructure
+  [tree]
+  (->> tree
+       transform
+       tree->map))
--- test/horizons/parser_test.clj
+++ test/horizons/parser_test.clj
@@ -72,26 +72,48 @@
           {:mean-radius "3389.9(2+-4)"
            :density "3.933(5+-4)"
            :mass "6.4185"}})))
-  ;(is (= (tree->map
-  ;         [:ephemeris
-  ;          [:line-item "first"]
-  ;          [:line-item "second"]
-  ;          [:line-item "third"]])
-  ;       {:ephemeris #{
-  ;                     {:line-item "first"}
-  ;                     {:line-item "second"}
-  ;                     {:line-item "third"}}})))
 
-(deftest set-of-maps-test
-  (is (= (set-of-maps
-            [:data [:k "x"]]
-            [:data [:k "y"]]
-            [:data [:k "z"]])
-         #{
-           {:data [:k "x"]}
-           {:data [:k "y"]}
-           {:data [:k "z"]}})))
+(def ephemeris-input
+  [ :ephemeris
+   [ :ephemeris-line-item
+    [ :measurement-time
+     [:year "2017"],
+     [:month "Feb"],
+     [:day "24"]
+     [:time "00:00"]]
+    [:ascension-declination "01 12 18.78 +07 36 55.3"],
+    [:apparent-magnitude "1.27"],
+    [:surface-brightness "4.29"],
+    [:range "2.00321056835551"],
+    [:range-rate "11.4427302"],
+    [:sun-observer-target-angle "44.2386"],
+    [:sun-observer-target-angle-direction "/T"],
+    [:sun-target-observer-angle "28.0789"]]])
 
-;(deftest parse-to-map-test
-;  (is (= (tree->map (get-edn "mars-results-parsed.edn"))
-;         (get-edn "mars-results-map.edn"))))
+(def ephemeris-output
+  {:ephemeris #{
+                {
+                 :measurement-time {
+                                    :year "2017"
+                                    :month "Feb"
+                                    :day "24"
+                                    :time "00:00"}
+                 :ascension-declination "01 12 18.78 +07 36 55.3"
+                 :apparent-magnitude "1.27"
+                 :surface-brightness "4.29"
+                 :range "2.00321056835551"
+                 :range-rate "11.4427302"
+                 :sun-observer-target-angle "44.2386"
+                 :sun-observer-target-angle-direction "/T"
+                 :sun-target-observer-angle "28.0789"}}})
+
+(deftest ephemeris-to-set-test
+  (is (= (transform ephemeris-input)
+         ephemeris-output)))
+
+
+(deftest transform-test
+  (is (= (restructure ephemeris-input)
+         ephemeris-output))
+  (is (= (restructure (get-edn "mars-results-parsed.edn"))
+         (get-edn "mars-results-map.edn"))))
