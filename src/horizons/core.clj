110c3e41177441dbbed185b785aa73a9422c2b6a
Robert Richter <robert.c.richter@gmail.com>
2017-03-11 15:07:58 -0600

    Wrap the main endpoint in some Liberator resource processing

--- src/horizons/core.clj
+++ src/horizons/core.clj
@@ -4,10 +4,28 @@
             [liberator.core :refer [resource defresource]]
             [ring.middleware.json :refer [wrap-json-body wrap-json-params wrap-json-response]]
             [ring.middleware.params :refer [wrap-params]]
-            [compojure.core :refer :all]))
+            [ring.util.response :refer [resource-response]]
+            [compojure.core :refer :all]
+            [compojure.route :as route]))
+
+(def supported-bodies
+  #{499})
+
+(defn supported? [id]
+  (contains? supported-bodies (int (bigdec id))))
+
+(defresource planetary-body-resource [id]
+  :allowed-methods [:get]
+  :available-media-types ["application/json"]
+  :exists? (fn [_] (supported? id))
+  :handle-ok (fn [ctx]
+               {:body (:S (restructure (parse (get-body id))))}))
 
 (defroutes handler
-           (GET "/bodies/:id" [id] {:body (:S (restructure (parse (get-body id))))}))
+           (GET "/" [] (resource-response "index.html" {:root "public"}))
+           (ANY ["/bodies/:id", :id #"[0-9]+"] [id] (planetary-body-resource id))
+           (route/resources "/"))
+
 
 (def app
   (-> handler
