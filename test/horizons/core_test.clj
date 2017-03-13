73ba7e2a4a8f3a6607e159a2af686ee72f4644cd
Robert Richter <robert.c.richter@gmail.com>
2017-03-13 16:38:56 -0500

    Write integration tests to parse all supported planets

--- test/horizons/core_test.clj
+++ test/horizons/core_test.clj
@@ -1,3 +1,13 @@
 (ns horizons.core-test
   (:require [clojure.test :refer :all]
-            [horizons.core :refer :all]))
+            [horizons.core :refer :all]
+            [instaparse.core :as insta]))
+
+(deftest get-mercury-test
+  (is (not (insta/failure? (get-planetary-body 199)))))
+
+(deftest get-earth-test
+  (is (not (insta/failure? (get-planetary-body 399)))))
+
+(deftest get-mars-test
+  (is (not (insta/failure? (get-planetary-body 499)))))
