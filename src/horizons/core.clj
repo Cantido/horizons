1b54dfda20bca925452da2c26811a505807a540d
Bobby Richter <robert.c.richter@gmail.com>
2017-03-15 12:27:14 -0500

    Document functions

--- src/horizons/core.clj
+++ src/horizons/core.clj
@@ -1,4 +1,5 @@
 (ns horizons.core
+  "Provides access to NASA's HORIZONS solar system body database."
   (:require [horizons.telnet-client :refer :all]
             [horizons.parser :refer :all]))
 
@@ -6,14 +7,17 @@
   #{199 399 499})
 
 (defn supported? [id]
+  "Check if the given body ID is definitely supported by this system."
   (contains? supported-bodies (int (bigdec id))))
 
-(defn throw-parse-exception
+(defn- throw-parse-exception
+  "Throw an exception documenting a parse exception"
   [body-string failure]
   (throw (Exception. (str "Unable to parse HORIZONS response. While parsing:\n\n" body-string
                           "\n\nGot the following failure: \n" (with-out-str (print failure))))))
 
 (defn get-planetary-body [id]
+  "Get geophysical data about a solar system body with the given ID."
   (let [body-string (get-body id)
         body-parse-tree (parse body-string)]
     (if (instaparse.core/failure? body-parse-tree)
