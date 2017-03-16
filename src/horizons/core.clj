4b17366721fed8c918302fe155a62e71ede406d1
Robert Richter <robert.c.richter@gmail.com>
2017-03-15 20:13:57 -0500

    Put docstrings in the correct place

--- src/horizons/core.clj
+++ src/horizons/core.clj
@@ -6,8 +6,9 @@
 (def supported-bodies
   #{199 399 499})
 
-(defn supported? [id]
+(defn supported?
   "Check if the given body ID is definitely supported by this system."
+  [id]
   (contains? supported-bodies (int (bigdec id))))
 
 (defn- throw-parse-exception
@@ -16,8 +17,9 @@
   (throw (Exception. (str "Unable to parse HORIZONS response. While parsing:\n\n" body-string
                           "\n\nGot the following failure: \n" (with-out-str (print failure))))))
 
-(defn get-planetary-body [id]
+(defn get-planetary-body
   "Get geophysical data about a solar system body with the given ID."
+  [id]
   (let [body-string (get-body id)
         body-parse-tree (parse body-string)]
     (if (instaparse.core/failure? body-parse-tree)
