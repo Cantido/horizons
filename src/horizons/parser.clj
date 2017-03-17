b03048a339b9af429a43636b74ab96f54a252462
Robert Richter <robert.c.richter@gmail.com>
2017-03-16 20:52:50 -0500

    Refactor and document

--- src/horizons/parser.clj
+++ src/horizons/parser.clj
@@ -12,8 +12,7 @@
   (Integer/parseInt s))
 
 (def ^:private transform-rules
-  {
-   :date (fn [& more] [:date (t/datemap->date (into {} more))])
+  {:date (fn [& more] [:date (t/datemap->date (into {} more))])
    :ephemeris (fn [& more] {:ephemeris (set more)})
    :ephemeris-line-item (fn [& more] (into {} more))
    :float bigdec
@@ -23,25 +22,44 @@
    :time (fn [& more]  {:time (into {} more)})
    :timestamp t/timestamp-transformer})
 
-;; If we could give insta/transform a default rule, it should
-;; be (fn [& rest] {:label (into {} rest)}). But alas...
-
 (defn transform
+  "Applies transformation functions to all nodes in the parse tree."
   [coll]
-  (if (core/failure? coll)
-    coll
-    (transform/transform transform-rules coll)))
+  (if-not (core/failure? coll)
+          (transform/transform transform-rules coll)
+          coll))
 
 (defn ^:private coll-of-colls?
+  "Returns true if every element of coll except the first is a collection."
   [coll]
   (and
-    (vector? coll)
-    (every? #(and (coll? %) (not (set? %))) (rest coll))))
+   (vector? coll)
+   (every? #(and (coll? %) (not (set? %))) (rest coll))))
 
 (defn ^:private into-map-or-nil
   "Applies `(into {} form)` if the argument is not nil. Otherwise returns nil."
   [coll]
   (when (seq coll) (into {} coll)))
+
+(defn put-keyword-in-ns
+  "If the given element is a keyword, stick that keyword into the current namespace.
+   Else, returns that element."
+  [form]
+  (if (keyword? form)
+    (keyword "horizons.core" (name form))
+    form))
+
+(defn tree-vec->map
+  "Transforms a list into a nested map, where the first list element is the key,
+  and the rest of the list is put into a map under that key."
+  [form]
+  (if (coll-of-colls? form)
+      {(first form) (into-map-or-nil (rest form))}
+      form))
+
+(def postwalk-step
+  "Transforms form into a nested map with namespaced keywords."
+  (comp tree-vec->map put-keyword-in-ns))
 
 (defn tree->map
   "Transforms a parse tree into a nested map, where the first entry in a non-leaf node becomes the key,
@@ -52,25 +70,15 @@
   and the remaining members are that node's children. This function will turn that tree into a
   map traversable by node names."
   [coll]
-  (if (core/failure? coll)
-    coll
-    (clojure.walk/postwalk
-      (comp
-        (fn [form]
-          (cond
-            (coll-of-colls? form) {(first form) (into-map-or-nil (rest form))}
-            :else form))
-        (fn [form]
-          (if (keyword? form)
-              (keyword "horizons.core" (name form))
-              form)))
-      coll)))
+  (if-not (core/failure? coll)
+          (clojure.walk/postwalk postwalk-step coll)
+          coll))
 
 (defn restructure
   "Applies tree transformations to a parse tree, then recursively converts all remaining key-value vectors into maps."
   [tree]
   (if (core/failure? tree)
-    tree
-    (->> tree
-         transform
-         tree->map)))
+      tree
+      (->> tree
+           transform
+           tree->map)))
