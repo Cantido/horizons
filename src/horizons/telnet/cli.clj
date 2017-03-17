b647b6c6bfb1a6860b7892db3622653c389cb002
Robert Richter <robert.c.richter@gmail.com>
2017-03-16 20:37:53 -0500

    Refactor and document the telnet code

--- src/horizons/telnet/cli.clj
+++ src/horizons/telnet/cli.clj
@@ -1,23 +1,27 @@
 (ns horizons.telnet.cli
+  "Some functions that are useful when debugging the Telnet connection in a REPL."
   (:require [clojure.core.async :as async]
+            [clojure.java.io :as io]
             [horizons.telnet.client :as client])
   (:import [java.io BufferedReader
                     InputStreamReader
                     BufferedWriter
                     OutputStreamWriter]))
 
-(defn pipe-stdin-to-telnet []
-  (let
-    [stdin (new BufferedReader (new InputStreamReader System/in))]
+(defn pipe-stdin-to-telnet
+  "Starts a loop in another thread that reads from stdin and writes it to the telnet service."
+  []
+  (let [stdin (io/reader *in*)]
     (async/go-loop []
              (let [got-from-stdin (.read stdin)]
-               (println "Got this from STDIN. Char: " (char got-from-stdin) ", int: " (int got-from-stdin) ", type: " (type got-from-stdin))
                (async/>! client/to-telnet got-from-stdin)
                (recur)))))
 
-(defn show-telnet-out []
+(defn show-telnet-out
+  "Starts a loop in another thread that reads from the Telnet service and writes it to stdout."
+  []
   (let
-    [stdout (new BufferedWriter (new OutputStreamWriter System/out))]
+    [stdout (io/writer *out*)]
     (async/go-loop []
              (.write stdout ^String (async/<! client/from-telnet))
              (.flush stdout)
--- src/horizons/telnet/client.clj
+++ src/horizons/telnet/client.clj
@@ -1,60 +1,73 @@
 (ns horizons.telnet.client
+  "Communicates to the HORIZONS Telnet server."
   (:require [clojure.core.async :as async]
-            [clojure.java.io :as io])
+            [clojure.java.io :as io]
+            [clojure.string :as string])
   (:import (org.apache.commons.net.telnet TelnetClient)
            (java.io BufferedReader BufferedWriter InputStreamReader OutputStreamWriter PrintStream)
            (java.nio.charset StandardCharsets Charset)))
 
-(def from-telnet (async/chan (async/sliding-buffer 1000)))
-(def to-telnet (async/chan))
+(def ^:private from-telnet (async/chan (async/sliding-buffer 1000)))
+(def ^:private to-telnet (async/chan))
 
-(defn connect
+(defn next-char
+  "Gets the next character from the given reader"
+  [rdr]
+  (-> rdr .read char str))
+
+(defn char-seq
+  "Returns a lazy sequence of single-character strings as read from the given reader."
+  [rdr]
+  (repeatedly #(next-char rdr)))
+
+(defn ^:private connect
   "Connects to the HORIZONS telnet service, attaching its input and output to channels."
   []
-  (let [client (new TelnetClient)]
+  (let [client (TelnetClient.)]
     (try
-      (.connect client "ssd.jpl.nasa.gov" 6775)
-      (let
-        [reader (io/reader (.getInputStream client) :encoding "US-ASCII")
-         writer (io/writer (.getOutputStream client) :encoding "US-ASCII")]
+     (.connect client "ssd.jpl.nasa.gov" 6775)
+     (let [writer (-> client .getOutputStream (io/writer :encoding "US-ASCII"))
+           reader-seq (-> client .getInputStream (io/reader :encoding "US-ASCII") char-seq)]
+       (try
+        (async/go-loop [remaining-reader-seq reader-seq]
+          (async/>! from-telnet (first remaining-reader-seq))
+          (recur (rest remaining-reader-seq)))
+        (async/go-loop []
+          (.write writer ^String (str (async/<! to-telnet) \newline))
+          (.flush writer)
+          (recur)))))))
 
-        (try
-          (async/go-loop []
-              (async/>! from-telnet (str (char (.read reader))))
-              (recur))
-          (async/go-loop []
-              (.write writer ^String (str (async/<! to-telnet) \newline))
-              (.flush writer)
-              (recur)))))))
-
-(defn next-token
+(defn ^:private next-token
+  "Get the next whitespace-delimited word from the Telnet connection"
   ([] (next-token ""))
   ([word-so-far]
    (let [next-char (async/<!! from-telnet)
-          next-word (str word-so-far next-char)]
-        (if (clojure.string/blank? next-char)
-            next-word
-            (recur next-word)))))
+         next-word (str word-so-far next-char)]
+     (if (string/blank? next-char)
+         next-word
+         (recur next-word)))))
 
-(defn next-block
+(defn ^:private next-block
   "Get everything from the HORIZONS client until the next input prompt"
   ([] (next-block ""))
   ([block-so-far]
    (let [next-word (next-token)]
      (cond
-       (clojure.string/starts-with? next-word "Horizons>") block-so-far
-       (clojure.string/starts-with? next-word "<cr>:") (str block-so-far next-word)
-       :else (recur (str block-so-far next-word))))))
+      (string/starts-with? next-word "Horizons>") block-so-far
+      (string/starts-with? next-word "<cr>:") (str block-so-far next-word)
+      :else (recur (str block-so-far next-word))))))
 
-(defn wait-for-prompt []
+(defn ^:private wait-for-prompt
+  "Blocks until the next input prompt is received from HORIZONS."
+  []
   (let [token (next-token)]
-       (if (not (clojure.string/starts-with? token "Horizons>"))
-           (recur))))
+    (when-not (clojure.string/starts-with? token "Horizons>") (recur))))
 
 (defn get-body
+  "Get a block of String data from the HORIZONS system about the given body-id"
   [body-id]
   (do
-    (connect)
-    (wait-for-prompt)
-    (async/>!! to-telnet body-id)
-    (next-block)))
+   (connect)
+   (wait-for-prompt)
+   (async/>!! to-telnet body-id)
+   (next-block)))
