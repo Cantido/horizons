5388e55c830b4b44132f3057a33edbd23d8c64c5
Robert Richter <robert.c.richter@gmail.com>
2017-03-26 20:28:37 -0500

    Organize tests a bit more descriptively

--- test/horizons/core_test.clj
+++ test/horizons/core_test.clj
@@ -3,11 +3,10 @@
             [horizons.core :refer :all]
             [instaparse.core :as insta]))
 
-(deftest get-mercury-test
-  (is (not (insta/failure? (get-planetary-body 199)))))
-
-(deftest get-earth-test
-  (is (not (insta/failure? (get-planetary-body 399)))))
-
-(deftest get-mars-test
-  (is (not (insta/failure? (get-planetary-body 499)))))
+(deftest get-planetary-body-integration-test
+  (testing "Getting Mercury (ID 199)"
+    (is (not (insta/failure? (get-planetary-body 199)))))
+  (testing "Getting Earth (ID 499)"
+    (is (not (insta/failure? (get-planetary-body 399)))))
+  (testing "Getting Mars (ID 599)"
+    (is (not (insta/failure? (get-planetary-body 499))))))
