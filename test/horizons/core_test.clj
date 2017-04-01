73bd7982f4e07a86eb992c6db1a46f11c05fb73b
Robert Richter <robert.c.richter@gmail.com>
2017-04-01 13:13:12 -0500

    Uncomment tests

--- test/horizons/core_test.clj
+++ test/horizons/core_test.clj
@@ -3,13 +3,13 @@
             [horizons.core :refer :all]
             [instaparse.core :as insta]))
 
-;(deftest get-planetary-body-integration-test
-;  (testing "Getting Mercury (ID 199)"
-;    (is (not (insta/failure? (get-planetary-body 199)))))
-;  (testing "Getting Earth (ID 499)"
-;    (is (not (insta/failure? (get-planetary-body 399)))))
-;  (testing "Getting Mars (ID 599)"
-;    (is (not (insta/failure? (get-planetary-body 499))))))
+(deftest get-planetary-body-integration-test
+  (testing "Getting Mercury (ID 199)"
+    (is (not (insta/failure? (get-planetary-body 199)))))
+  (testing "Getting Earth (ID 499)"
+    (is (not (insta/failure? (get-planetary-body 399)))))
+  (testing "Getting Mars (ID 599)"
+    (is (not (insta/failure? (get-planetary-body 499))))))
 
 (deftest get-ephemeris-integration-test
   (testing "Getting Mercury (ID 199)"
