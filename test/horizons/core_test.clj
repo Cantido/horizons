59a013f25d3ba04470fa8e1e12e7fa983e9d37ad
Robert Richter <robert.c.richter@gmail.com>
2017-08-16 11:15:26 -0500

    Refactor core test a little

--- test/horizons/core_test.clj
+++ test/horizons/core_test.clj
@@ -12,5 +12,8 @@
     (is (not (insta/failure? (get-planetary-body 499))))))
 
 (deftest get-ephemeris-integration-test
-  (testing "Getting Mercury (ID 199)"
-    (is (not (insta/failure? (get-ephemeris 199))))))
+  (testing "Getting Mercury (ID 199) with default parameters"
+    (let [response (get-ephemeris 199)]
+      (is (not (insta/failure? response)))
+      (is (not (nil? response)))
+      (is (not (nil? (get-in response [:horizons.core/time-frame])))))))
