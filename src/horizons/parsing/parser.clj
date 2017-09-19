78740906f9e92b02d084065f238218cecd97b388
Robert Richter <robert.c.richter@gmail.com>
2017-09-18 20:41:45 -0500

    Style cleanup

--- src/horizons/parsing/parser.clj
+++ src/horizons/parsing/parser.clj
@@ -37,9 +37,8 @@
     bigdec))
 
 (defn sci-not->bigdec [significand mantissa]
-  (if (not-any? nil? [significand mantissa])
-    (.scaleByPowerOfTen (bigdec significand) mantissa)
-    nil))
+  (when (not-any? nil? [significand mantissa])
+    (.scaleByPowerOfTen (bigdec significand) mantissa)))
 
 (defn value-with-exponent-map->bigdec
   ([label & more]
