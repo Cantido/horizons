f09e6277eae858f1e432294b4ba8afdcc4a34372
Bobby Richter <robert.c.richter@gmail.com>
2017-09-23 11:11:23 -0500

    Turn the web server into a true component

--- src/horizons/web.clj
+++ src/horizons/web.clj
@@ -9,57 +9,50 @@
             [liberator.core :as liberator]
             [ring.middleware.defaults :as defaults]
             [ring.util.response :as response]
-            [horizons.parsing.time :as t])
-  (:import (org.joda.time DateTime)))
+            [horizons.parsing.time :as t]))
 
-; Since liberator/defresource is a macro, some editors
-; (like IntelliJ) will think that the symbols you give
-; them are undefined. This declare statement just lets
-; my IDE shut up; the symbols ARE declared.
-(declare planetary-body-resource ephemeris-resource id)
-
-(defn handle-exception [e]
+(defn- handle-exception [horizons-client e]
   (log/error e)
   (-> "500.json"
       (response/resource-response {:root "public"})
       (response/status 500)
       (liberator.representation/ring-response)))
 
-(liberator/defresource planetary-body-resource [id]
-  :allowed-methods [:get]
-  :available-media-types ["application/json"]
-  :available-languages ["en-US"]
-  :exists? (fn [_] (horizons/supported? id))
-  :handle-ok (fn [_] (horizons/get-planetary-body id))
-  :handle-exception handle-exception)
+(defn- planetary-body-resource [horizons-client id]
+  (liberator/resource
+    :allowed-methods [:get]
+    :available-media-types ["application/json"]
+    :available-languages ["en-US"]
+    :exists? (fn [_] (horizons/supported? id))
+    :handle-ok (fn [_] (horizons/get-planetary-body id))
+    :handle-exception handle-exception))
 
-(defn process-params [m]
-  (-> m
-    (select-keys #{:start :stop})
-    (update :start t/normalize-date-string)
-    (update :end t/normalize-date-string)))
+(defn- ephemeris-resource [horizons-client id]
+  (liberator/resource
+    :allowed-methods [:get]
+    :available-media-types ["application/json"]
+    :available-languages ["en-US"]
+    :exists? (fn [_] (horizons/supported? id))
+    :handle-ok (fn [ctx] (horizons/get-ephemeris id))
+    :handle-exception handle-exception))
 
-(liberator/defresource ephemeris-resource [id]
-  :allowed-methods [:get]
-  :available-media-types ["application/json"]
-  :available-languages ["en-US"]
-  :exists? (fn [_] (horizons/supported? id))
-  :handle-ok (fn [ctx] (horizons/get-ephemeris id))
-  :handle-exception handle-exception)
+(defn- app-routes [horizons-client]
+  (routes/routes
+    (routes/GET "/" [] (response/redirect "https://cantido.github.io/horizons/"))
+    (routes/context ["/bodies/:id", :id #"[0-9]+"] [id]
+      (routes/ANY "/" [] (planetary-body-resource horizons-client id))
+      (routes/ANY "/ephemeris" [] (ephemeris-resource horizons-client id)))
+    (route/not-found (response/not-found "Resource not found."))))
 
-(routes/defroutes handler
-  (routes/GET "/" [] (response/redirect "https://cantido.github.io/horizons/"))
-  (routes/context ["/bodies/:id", :id #"[0-9]+"] [id]
-    (routes/ANY "/" [] (planetary-body-resource id))
-    (routes/ANY "/ephemeris" [] (ephemeris-resource id)))
-  (route/not-found (response/not-found "Resource not found.")))
-
-(def app
-  (-> handler
+(defn- app-handler [horizons-client]
+  (-> (app-routes horizons-client)
       (defaults/wrap-defaults
         (assoc defaults/api-defaults :static {:resources "public"}))))
 
-(defrecord WebServer [http-server]
+(def app (app-handler {}))
+
+
+(defrecord WebServer [http-server horizons-client]
   component/Lifecycle
   (start [this]
     (assoc this :http-server
@@ -67,7 +60,7 @@
         (log/info "Starting HORIZONS on port" port)
         (log/debug "DEBUG logging enabled")
         (log/trace "TRACE logging enabled")
-        (web/run app
+        (web/run (app-handler horizons-client)
                  :host "0.0.0.0"
                  :port port
                  :path "/"))))
