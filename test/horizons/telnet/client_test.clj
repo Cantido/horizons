9770af26651c75b0497ebf3a4127dd6017629b62
Bobby Richter <robert.c.richter@gmail.com>
2017-10-08 16:49:59 -0500

    Refactor test structure to add some docs & tests

--- test/horizons/telnet/client_test.clj
+++ test/horizons/telnet/client_test.clj
@@ -14,70 +14,64 @@
   ([to-telnet from-telnet] (:telnet-client (test/build-test-system to-telnet from-telnet))))
 
 (deftest next-token-test
-  (is (asu/closed? (client/next-token asu/closed-chan)))
-  (is (asu/closed? (client/next-token (async/to-chan ""))))
+  (testing "returns a closed channel when given a closed or empty channel"
+    (is (asu/closed? (client/next-token asu/closed-chan)))
+    (is (asu/closed? (client/next-token (async/to-chan "")))))
   (is (= "word " (async/<!! (client/next-token (async/to-chan "word ")))))
   (let [token-chan (async/to-chan "first second third ")]
     (is (= ["first " "second " "third "] (repeatedly 3 #(async/<!! (client/next-token token-chan)))))))
 
-(deftest next-block
-  (is (asu/closed? (client/next-block asu/closed-chan))))
+(deftest next-block-test
+  (testing "returns a closed channel when given a closed or empty channel"
+    (is (asu/closed? (client/next-block asu/closed-chan)))
+    (is (asu/closed? (client/next-block (async/to-chan "")))))
+  (testing "returns everything up to the next block"
+    (is (= "word Horizons> " (async/<!! (client/next-block (async/to-chan "word Horizons> word")))))))
 
-(deftest wait-for-prompt
-  (is (asu/closed? (client/wait-for-prompt asu/closed-chan))))
+(deftest wait-for-prompt-test
+  (is (asu/closed? (client/wait-for-prompt asu/closed-chan)))
+  (is (asu/closed? (client/wait-for-prompt (async/to-chan "Horizons>")))))
 
-(deftest swallow-echo
+(deftest swallow-echo-test
   (testing "returns false if the channel was closed before it could swallow the entire string"
     (is (false? (client/swallow-echo "s" asu/closed-chan)))
     (is (false? (client/swallow-echo "12345" (async/to-chan "1\r\n")))))
   (testing "returns true if the entire echo could be consumed"
     (is (true? (client/swallow-echo "s" (async/to-chan "s\r\n"))))))
 
-(deftest reset-client
+(deftest reset-client-test
   (is (false? (client/reset-client asu/closed-chan))))
 
-(deftest connect
-  (is (vector? (client/connect
-                 (client/new-telnet-client)
-                 [asu/closed-chan asu/closed-chan]))))
+(deftest connect-test
+  (is (vector? (client/connect (component))))
+  (is (vector? (client/connect (component "full-geophysical-interaction.txt")))))
 
-(deftest wait-for-prompt
-  (is (asu/closed? (client/wait-for-prompt asu/closed-chan)))
-  (is (asu/closed? (client/wait-for-prompt (async/to-chan "Horizons>")))))
-
-(deftest transmit
+(deftest transmit-test
   (testing "returns the next block"
     (is (= "Horizons> " (client/transmit
-                          (client/new-telnet-client)
-                          (async/chan)
-                          (async/to-chan "s\r\nHorizons> ") "s"))))
+                               (component)
+                               (asu/dropping-channel)
+                               (async/to-chan "s\r\nHorizons> ") "s"))))
   (testing "returns false if either channels were closed"
       (is (false? (client/transmit
-                    (client/new-telnet-client)
+                    (component)
                     asu/closed-chan
                     asu/closed-chan
                     "s")))
       (is (false? (client/transmit
-                    (client/new-telnet-client)
+                    (component)
                     asu/closed-chan
                     (async/to-chan "s\r\nHorizons> ")
                     "s")))
       (is (false? (client/transmit
-                    (client/new-telnet-client)
-                    (async/chan)
+                    (component)
+                    (asu/dropping-channel)
                     asu/closed-chan
                     "s")))))
 
-(def full-geo-text
-  (slurp (io/file (io/resource "full-geophysical-interaction.txt"))))
-
-(def full-ephem-text
-  (slurp (io/file (io/resource "full-ephem-interaction.txt"))))
-
-(deftest get-body
-  (let [result (client/get-body
-                 (client/new-telnet-client)
-                 [(async/chan) (async/to-chan full-geo-text)] 199)]
+(deftest get-body-test
+  (let [result (client/get-body (component "full-geophysical-interaction.txt") 199)]
+    (is (some? result))
     (is (string/includes? result "Mean radius (km)      =  2440(+-1)"))))
 
 (deftest get-ephemeris-test
