8d679a0980b163703f060f0c8d23c1cb15edaf8f
Bobby Richter <robert.c.richter@gmail.com>
2017-10-08 18:11:21 -0500

    Test the webserver component
    This actually made clear to me that we were passing around
    the horizons client, not the web server component.
    This commit also clarifies which component we should be passing.

--- src/horizons/web.clj
+++ src/horizons/web.clj
@@ -10,41 +10,46 @@
             [ring.util.response :as response]
             [horizons.parsing.time :as t]))
 
-(defn- handle-exception [horizons-client e]
+(defn- handle-exception [web-server-component e]
   (log/error e)
   (-> "500.json"
       (response/resource-response {:root "public"})
       (response/status 500)
       (liberator.representation/ring-response)))
 
-(defn- planetary-body-resource [horizons-client id]
+(defn- planetary-body-resource [web-server-component id]
   (liberator/resource
     :allowed-methods [:get]
     :available-media-types ["application/json"]
     :available-languages ["en-US"]
-    :exists? (fn [_] (horizons/supported? horizons-client id))
-    :handle-ok (fn [_] (horizons/get-planetary-body horizons-client id))
-    :handle-exception (partial handle-exception horizons-client)))
+    :exists? (fn [_] (horizons/supported? (:horizons-client web-server-component) id))
+    :handle-ok (fn [_] (horizons/get-planetary-body (:horizons-client web-server-component) id))
+    :handle-exception (partial handle-exception web-server-component)))
 
-(defn- ephemeris-resource [horizons-client id]
+(defn- ephemeris-options [horizons-client m]
+  (-> m
+    (get-in [:request :params])
+    (select-keys #{:start :end :step-size})))
+
+(defn- ephemeris-resource [web-server-component id]
   (liberator/resource
     :allowed-methods [:get]
     :available-media-types ["application/json"]
     :available-languages ["en-US"]
-    :exists? (fn [_] (horizons/supported? horizons-client id))
-    :handle-ok (fn [ctx] (horizons/get-ephemeris horizons-client id))
-    :handle-exception (partial handle-exception horizons-client)))
+    :exists? (fn [_] (horizons/supported? (:horizons-client web-server-component) id))
+    :handle-ok (fn [ctx] (horizons/get-ephemeris (:horizons-client web-server-component) id (ephemeris-options ephemeris-resource ctx)))
+    :handle-exception (partial handle-exception web-server-component)))
 
-(defn- app-routes [horizons-client]
+(defn- app-routes [web-server-component]
   (routes/routes
     (routes/GET "/" [] (response/redirect "https://cantido.github.io/horizons/"))
     (routes/context ["/bodies/:id", :id #"[0-9]+"] [id]
-      (routes/ANY "/" [] (planetary-body-resource horizons-client id))
-      (routes/ANY "/ephemeris" [] (ephemeris-resource horizons-client id)))
+      (routes/ANY "/" [] (planetary-body-resource web-server-component id))
+      (routes/ANY "/ephemeris" [] (ephemeris-resource web-server-component id)))
     (route/not-found (response/not-found "Resource not found."))))
 
-(defn app-handler [horizons-client]
-  (-> (app-routes horizons-client)
+(defn app-handler [web-server-component]
+  (-> (app-routes web-server-component)
       (defaults/wrap-defaults
         (assoc defaults/api-defaults :static {:resources "public"}))))
 
@@ -59,7 +64,7 @@
         (log/info "Starting HORIZONS on port" port)
         (log/debug "DEBUG logging enabled")
         (log/trace "TRACE logging enabled")
-        (web/run (app-handler horizons-client)
+        (web/run (app-handler this)
                  :host "0.0.0.0"
                  :port port
                  :path "/"))))
--- test/horizons/web_test.clj
+++ test/horizons/web_test.clj
@@ -1,3 +1,28 @@
 (ns horizons.web-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [clojure.core.async :as async]
+            [clojure.java.io :as io]
+            [horizons.main :as main]
+            [horizons.web :as web]
+            [horizons.test-utils :as test]
+            [com.stuartsierra.component :as component]
+            [ring.mock.request :as mock]))
 
+(defn component [s]
+  (:web-server (test/build-test-system s)))
+
+(deftest web-test
+  (testing "get ephemeris"
+    (testing "with default options"
+      (let [web-server-component (component "full-ephem-interaction.txt")
+            app (web/app-handler web-server-component)
+            response (app (mock/request :get "/bodies/499/ephemeris"))]
+        (is (= (:status response) 200))
+        (is (not (empty? (:body response)))))))
+  (testing "get geophysical data"
+    (testing "with default options"
+      (let [web-server-component (component "full-geophysical-interaction.txt")
+            app (web/app-handler web-server-component)
+            response (app (mock/request :get "/bodies/499"))]
+        (is (= (:status response) 200))
+        (is (not (empty? (:body response))))))))
