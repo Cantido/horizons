14d1fb760bd6c5797d9af1d316b8d685e3a382f0
Bobby Richter <robert.c.richter@gmail.com>
2017-10-16 12:29:45 -0500

    Test start/end/step params to web component

--- test/horizons/web_test.clj
+++ test/horizons/web_test.clj
@@ -18,7 +18,18 @@
             app (web/app-handler web-app-component)
             response (app (mock/request :get "/bodies/499/ephemeris"))]
         (is (= (:status response) 200))
-        (is (not (empty? (:body response)))))))
+        (is (not (empty? (:body response))))))
+    (testing "with specified start, stop, and step time"
+      (let [web-app-component (component "full-ephem-with-dates.txt")
+            app (web/app-handler web-app-component)
+            response (app (mock/request :get "/bodies/499/ephemeris" {:start "1990-10-27T22:17"
+                                                                      :end "1990-10-28T22:17"
+                                                                      :step-size "1h"}))]
+        (is (= (:status response) 200))
+        (is (not (empty? (:body response))))
+        (is (= true (clojure.string/includes? (:body response) "timestamp\":\"1990-10-27")))
+        (is (= true (clojure.string/includes? (:body response) "timestamp\":\"1990-10-28")))
+        (is (= true (clojure.string/includes? (:body response) "duration\":{\"minutes\":60"))))))
   (testing "get geophysical data"
     (testing "with default options"
       (let [web-app-component (component "full-geophysical-interaction.txt")
