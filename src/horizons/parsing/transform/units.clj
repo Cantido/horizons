d4e565bdcb0579e6d636e4346f54a99fd383ecf8
Rosa Richter <rosa@rosa.solar>
2019-07-15 17:31:40 -0500

    Record normalizations in comments
    I plan on implementing normalization.

--- src/horizons/parsing/transform/units.clj
+++ src/horizons/parsing/transform/units.clj
@@ -6,18 +6,18 @@
   ([s t] (constantly {:unit-code s :unit-text t})))
 
 (def transform-rules
-  {:unit-23 (unit-code "23" "g/cm³")
-   :unit-2A (unit-code "2A" "rad/s")
-   :unit-A62 (unit-code "A62" "erg/g·s")
-   :unit-BAR (unit-code "BAR" "bar")
-   :unit-D54 (unit-code "D54" "W/m²")
-   :unit-D61 (unit-code "D61" "'")
-   :unit-D62 (unit-code "D62" "\"")
-   :unit-DD (unit-code "DD" "°") ;
-   :unit-H20 (unit-code "H20" "km³")
-   :unit-KEL (unit-code "KEL" "K")
-   :unit-KGM (unit-code "KGM" "kg")
-   :unit-KMT (unit-code "KMT" "km")
-   :unit-M62 (unit-code "M62" "km/s")
-   :unit-MSK (unit-code "MSK" "m/s²")
-   :unit-SEC (unit-code "SEC" "s")})
+  {:unit-23 (unit-code "23" "g/cm³") ; already normalized
+   :unit-2A (unit-code "2A" "rad/s") ; 10³ kg/m³, code: 28
+   :unit-A62 (unit-code "A62" "erg/g·s") ; E-4 W/kg
+   :unit-BAR (unit-code "BAR" "bar") ; 0⁵ Pa, code: PAL
+   :unit-D54 (unit-code "D54" "W/m²") ; already normalized
+   :unit-D61 (unit-code "D61" "'") ; 2,908 882 x 10⁻⁴ rad code: C81
+   :unit-D62 (unit-code "D62" "\""); 4,848 137 x 10⁻⁶ rad, code: C81
+   :unit-DD (unit-code "DD" "°") ; 1,745 329 x 10⁻² rad, code: C81
+   :unit-H20 (unit-code "H20" "km³") ; 10⁹ m³, code: MTQ
+   :unit-KEL (unit-code "KEL" "K") ; already normalized
+   :unit-KGM (unit-code "KGM" "kg") ; already normalized
+   :unit-KMT (unit-code "KMT" "km") ; 10³ m, code: MTR
+   :unit-M62 (unit-code "M62" "km/s"); 10³ m/s, code: MTS
+   :unit-MSK (unit-code "MSK" "m/s²") ; already normalized
+   :unit-SEC (unit-code "SEC" "s")}) ; already normalized
