a952707d99b356eb1e6446e41d75b0e720280435
Rosa Richter <rosa@rosa.solar>
2019-07-15 12:46:52 -0500

    Put the actual human-readable units in comments
    I plan on adding them all as unit-text so this is temporary but helpful.

--- src/horizons/parsing/transform/units.clj
+++ src/horizons/parsing/transform/units.clj
@@ -7,17 +7,17 @@
 
 (def transform-rules
   {:unit-23 (unit-code "23" "g/cm³")
-   :unit-2A (unit-code "2A")
-   :unit-A62 (unit-code "A62")
-   :unit-BAR (unit-code "BAR")
-   :unit-D54 (unit-code "D54")
-   :unit-D61 (unit-code "D61")
-   :unit-D62 (unit-code "D62")
-   :unit-DD (unit-code "DD")
-   :unit-H20 (unit-code "H20")
-   :unit-KEL (unit-code "KEL")
-   :unit-KMT (unit-code "KMT")
-   :unit-KGM (unit-code "KGM")
-   :unit-MSK (unit-code "MSK")
-   :unit-M62 (unit-code "M62")
-   :unit-SEC (unit-code "SEC")})
+   :unit-2A (unit-code "2A") ;rad/s
+   :unit-A62 (unit-code "A62") ;erg/g·s
+   :unit-BAR (unit-code "BAR") ;bar
+   :unit-D54 (unit-code "D54") ;W/m²
+   :unit-D61 (unit-code "D61") ;'
+   :unit-D62 (unit-code "D62") ;"
+   :unit-DD (unit-code "DD") ;°
+   :unit-H20 (unit-code "H20") ;km³
+   :unit-KEL (unit-code "KEL") ;K
+   :unit-KGM (unit-code "KGM") ;kg
+   :unit-KMT (unit-code "KMT") ;km
+   :unit-M62 (unit-code "M62") ;km/s
+   :unit-MSK (unit-code "MSK") ;m/s²
+   :unit-SEC (unit-code "SEC")}) ;s
